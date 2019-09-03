66d8c4bcbe9d61cec745069061c5cd88ddec4e61
dustincalla1 <46655710+dustincalla1@users.noreply.github.com>
2019-09-03 10:32:25 -0700

    net_server

---
+++ net_server.py
@@ -0,0 +1,15 @@
+import socket
+ 
+
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #choose_a_port_number = int(input())
+    addr = ("0.0.0.0", 40000)
+    sock.bind(addr)
+    sock.listen(5)
+    (connectedSock, clientAddress) = sock.accept()
+    msg = sock.recv(1024).decode()
+    sock.sendall(message.encode())
+        
+
+main()
