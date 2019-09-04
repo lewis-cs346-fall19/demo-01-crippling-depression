dd05837885661970381054c7705eee662bdd2937
ClintonKral <46661683+ClintonKral@users.noreply.github.com>
2019-09-04 11:50:14 -0700

    Add files via upload

--- net_client.py
+++ net_client.py
@@ -9,7 +9,9 @@ def main():
     for i in range(1, 101):
         print(i)
         sock.sendall(str(i).encode())
-        sock.recv(100)
+        reply = sock.recv(100).decode()
+        print(reply)
+        
 
 main()
     
