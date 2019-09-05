df8f93412e2b389875870683ebc01f03ca80f01e
dustincalla1 <46655710+dustincalla1@users.noreply.github.com>
2019-09-04 17:30:06 -0700

    the backup file

--- net_server.py
+++ net_server.py
@@ -4,19 +4,17 @@ def main():
    addr = ("0.0.0.0", 40000)
    sock.bind(addr)
    sock.listen(5)
-   #count = 0 
+   count = 0 
    while True:
       (connectedSock, clientAddress) = sock.accept()
 
       while True:
-         
+
          try:
             message = connectedSock.recv(1024).decode()
             #msg = "WoW Queue: " + message
             connectedSock.sendall(("WoW Queue: " + message).encode())
             count += 1
-            if len(message) == 0:
-               connectedSock.close()
 
          except ConnectionAbortedError:
             connectedSock.close()
